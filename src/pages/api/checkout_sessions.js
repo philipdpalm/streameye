1e9bd5f13aa1b67a6a6d9a1f6c59d620f78c6e7a
philipdpalm <user@Users-MacBook-Air.local>
2023-03-03 21:41:34 +0000

    update stripe env

--- src/pages/api/checkout_sessions.js
+++ src/pages/api/checkout_sessions.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: 'price_1MheUoFACH6Ord31xmK4T5La',
+            price: process.env.PRICE_ID,
             quantity: 1,
           },
         ],
